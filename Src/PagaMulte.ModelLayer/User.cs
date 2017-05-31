aa21803214df5e2f10e965c02cdd148fee283742
AntonioSciarretta <a.sciarretta@live.com>
2017-05-31 11:09:32 +0200

    minor changes

--- Src/PagaMulte.ModelLayer/User.cs
+++ Src/PagaMulte.ModelLayer/User.cs
@@ -5,7 +5,7 @@ namespace PagaMulte.ModelLayer
     public class User
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Name { get; set; }
         public string Password { get; set; }
         public string Token { get; set; }
         public DateTime DataExpired { get; set; } 
